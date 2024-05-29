ad4d7ef640f0c97d696f5b80dbd08c4a383b59c4
Prasad Mahabare <prasadmahabare7@outlook.com>
2024-05-29 14:16:08 -0400

    Completed Backend Setup

--- CloudChatApp/.wundergraph/wundergraph.config.ts
+++ CloudChatApp/.wundergraph/wundergraph.config.ts
@@ -1,41 +1,53 @@
-import { authProviders, configureWunderGraphApplication, cors, introspect, templates } from '@wundergraph/sdk';
+import {
+	authProviders,
+	configureWunderGraphApplication,
+	cors,
+	EnvironmentVariable,
+	introspect,
+	templates,
+} from '@wundergraph/sdk';
 import { NextJsTemplate } from '@wundergraph/nextjs/dist/template';
 import server from './wundergraph.server';
 import operations from './wundergraph.operations';
 
-const spaceX = introspect.graphql({
-	apiNamespace: 'spacex',
-	url: 'https://spacex-api.fly.dev/graphql/',
-});
+
+const fauna = introspect.graphql({
+	apiNamespace: 'db',
+	url: new EnvironmentVariable('FAUNADB_GRAPHQL_URL'),
+	headers: (builder) => {
+	  builder.addStaticHeader(
+		'Authorization',
+		new EnvironmentVariable('FAUNADB_TOKEN')
+	  )
+	  return builder
+	},
+  })
 
 // configureWunderGraph emits the configuration
 configureWunderGraphApplication({
-	apis: [spaceX],
+	apis: [fauna],
 	server,
 	operations,
-	generate: {
-		codeGenerators: [
-			{
-				templates: [new NextJsTemplate()],
-				path: '../components/generated',
-			},
-		],
-	},
+	codeGenerators: [
+		{
+			templates: [...templates.typescript.all],
+		},
+		{
+			templates: [new NextJsTemplate()],
+			path: '../components/generated',
+		},
+	],
 	cors: {
 		...cors.allowAll,
-		allowedOrigins: process.env.NODE_ENV === 'production' ? ['https://*'] : ['http://*'],
-		/**
-		 * Please configure CORS carefully to make sure that your users are protected.
-		 * Allowing all origins is usually the worst possible configuration.
-		 *
-		 * @docs https://docs.wundergraph.com/docs/wundergraph-config-ts-reference/configure-cors
-		 */
-		// allowedOrigins: process.env.NODE_ENV === 'production' ? ['http://your.app'] : ['http://localhost:3000'],
+		allowedOrigins: process.env.NODE_ENV === 'production' ? ['http://localhost:3000'] : ['http://localhost:3000'],
 	},
 	authentication: {
 		cookieBased: {
 			providers: [authProviders.demo()],
 			authorizedRedirectUris: ['http://localhost:3000'],
+			secureCookieHashKey: new EnvironmentVariable('WUNDERGRAPH_SECURE_COOKIE_HASH_KEY'), // must be of length 32
+			secureCookieBlockKey: new EnvironmentVariable('WUNDERGRAPH_SECURE_COOKIE_BLOCK_KEY'), // must be of length 32
+			csrfTokenSecret: new EnvironmentVariable('WUNDERGRAPH_CSRF_TOKEN_SECRET'), // must be of length 11
 		},
 	},
 	security: {
